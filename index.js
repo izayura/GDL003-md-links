43cd330f6d5a9155feae20cfb326c6288e48c7ca
Gloria <izayura@gmail.com>
2019-08-16 17:36:47 -0500

    Se comenzaron a escribir los test, se nombraron algunas funciones, se actualizó el package.json

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-module.exports = () => {
-  // ...
+const path = require('path');
+
+findMD = (filePath) =>{
+  return path.extname(filePath) === '.md' ? true:false;
+
 };
+
+findLinks = () => {
+
+};
+
+printLinks = () => {
+
+};
+
+validateLinks = () => {
+
+};
+
+/* module.exports = (filePath) => {
+  return path.extname(filePath) === '.md' ? true:false;
+
+}; */
+
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,57 @@
-const mdLinks = require('../');
+const mdLinks = require('../index.js');
 
 
-describe('mdLinks', () => {
+describe('findMD', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
+  it('is a function', () => {
+    expect(typeof findMD).toBe('function');
   });
 
+  it('should return TRUE for a valid .md file', () => {
+    expect(findMD('../README.md')).toBe(true);
+    console.log('You have a valid .md file');
+  });
+
+  it('should return FALSE for an invalid .md file', () => {
+    expect(findMD('../README.js')).toBe(false);
+    console.log('You have an invalid .md file');
+  });
+
+});
+
+describe('findLinks', () => {
+
+  it('is a function', () => {
+    expect(typeof findLinks).toBe('function');
+  });
+
+  it('should return if there are any link in the .md file', () => {
+    expect(findLinks(filePath)).toBe('https://github.com/izayura/GDL003-md-links.git');
+  });
+});
+
+describe('printLinks', () => {
+
+  it('is a function', () => {
+    expect(typeof printLinks).toBe('function');
+  });
+
+  it('should print in terminal the links founded in the .md file', () => {
+    expect(printLinks(filePath)).toBe('https://github.com/izayura/GDL003-md-links.git');
+  });
+});
+
+describe('validateLinks', () => {
+
+  it('is a function', () => {
+    expect(typeof validateLinks).toBe('function');
+  });
+
+  it('the link is validate', () => {
+    expect(validateLinks()).toBe(true);
+  });
+
+  it('the link is not validate', () => {
+    expect(validateLinks()).toBe(false);
+  });
 });
